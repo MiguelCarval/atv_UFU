0e3f9886975d5c4e4b0dc00a2457b5897b97c6cd
Miguel <carvalhoajqs@gmail.com>
2025-05-10 11:43:32 -0300

    atividade 31 adicionada

--- atv_vetores/atv_31.php
+++ atv_vetores/atv_31.php
@@ -1,7 +1,31 @@
- 
- 
+ <!--Atividade 31: Vetores -->
 <?php
 
+$V1 = [];
+$V2 = [];
+$V3 = [];
 
+echo "Digite os valores do vetor 1: \n";
+
+for($i = 0; $i < 10; $i++){
+
+    $V1[$i] = (int)readline();
+}
+
+echo "Digite os valores do vetor 2: \n";
+
+for($i = 0; $i < 10; $i++){
+
+    $V2[$i] = (int)readline();
+}
+
+
+foreach($V1 as $valores){
+    if(!in_array($valores, $V2)){
+        $V3[] = $valores;
+    }
+}
+
+print_r($V3);
 
 ?>
