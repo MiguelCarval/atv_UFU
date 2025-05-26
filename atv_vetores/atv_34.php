bb21a264bc55bf4eb16d77e8d2f21cc802681226
Miguel <carvalhoajqs@gmail.com>
2025-05-26 10:12:04 -0300

    Atividade 34 enviada

---
+++ atv_vetores/atv_34.php
@@ -0,0 +1,24 @@
+<!--Atividade 33: Vetores -->
+<?php
+
+$V = [];
+
+echo "Digite 10 valores DIFERENTES para o vetor \n";
+
+while(count($V) < 10){
+
+    $valores = (int)readline();
+
+    if(in_array($valores, $V)){
+        echo "pfv digite outro valor diferente \n";
+    }else{
+        $V[] = $valores;
+    }
+
+}
+
+
+echo "Vetor final: \n";
+print_r($V);
+
+?>
